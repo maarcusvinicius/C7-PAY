14db6d948ce9c19f695f0b465e37dd86cf50446e
maarcusvinicius <marcus.editor77@gmail.com>
2022-10-20 14:34:56 -0300

    manipulation DOM

--- src/main.js
+++ src/main.js
@@ -22,12 +22,22 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
-// Security Code
+// Security Code CVC
 const securityCode = document.querySelector("#security-code");
 const securityCodePattern = {
   mask: "0000",
 };
 const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+});
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value");
+
+  ccSecurity.innerText = code.length === 0 ? "123" : code;
+}
 
 // Expiration Date
 const expirationDate = document.querySelector("#expiration-date");
@@ -47,6 +57,16 @@ const expirationDatePattern = {
   },
 };
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
 
 // Card Number
 const cardNumber = document.querySelector("#card-number");
@@ -73,9 +93,39 @@ const cardNumberPattern = {
       return number.match(item.regex);
     });
 
-    console.log(foundMask)
+    // console.log(foundMask);
 
-    return foundMask
+    return foundMask;
   },
 };
-const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number){
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+// Adicionar Cartão
+const addButton = document.querySelector("#add-card");
+addButton.addEventListener("click", () => {
+  alert("Catão adicionado!");
+});
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+// Nome do Titular
+const cardHolder = document.querySelector("#card-holder");
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value");
+
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULLANO DA SILVA" : cardHolder.value;
+});
